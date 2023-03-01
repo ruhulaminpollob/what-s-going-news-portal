44f87f6c997bee85ad3a756cf1d980a34cc540b7
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-01 16:40:37 +0600

    category news data loaded

--- scripts/index.js
+++ scripts/index.js
@@ -9,12 +9,17 @@ const loadCategories = () => {
 
 displayCategories=(data)=>{
     const categoriesContainer=document.getElementById('category-container');
-    
     data.news_category.forEach(category => {
-        
-        categoriesContainer.innerHTML+=`<a class="nav-link" href="#">${category?.category_name}</a>`
-
+        categoriesContainer.innerHTML+=`<a class="nav-link" href="#" onclick="loadCategoryNews('${category.category_id}')">${category?.category_name}</a>`
         
     });
+
+}
+// category news 
+const loadCategoryNews=(categoryId)=>{
+    const URL=`https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    fetch(URL)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
 
 }
