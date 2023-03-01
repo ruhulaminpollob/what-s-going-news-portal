63182156013e427fed1f107d752d5f15d224f688
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-01 19:46:56 +0600

    sort section added

--- scripts/index.js
+++ scripts/index.js
@@ -10,16 +10,24 @@ const loadCategories = () => {
 displayCategories=(data)=>{
     const categoriesContainer=document.getElementById('category-container');
     data.news_category.forEach(category => {
-        categoriesContainer.innerHTML+=`<a class="nav-link" href="#" onclick="loadCategoryNews('${category.category_id}')">${category?.category_name}</a>`
+        categoriesContainer.innerHTML+=`<a class="nav-link" href="#" onclick="loadCategoryNews('${category.category_id}','${category.category_name}')">${category?.category_name}</a>`
         
     });
 
 }
 // category news 
-const loadCategoryNews=(categoryId)=>{
+const loadCategoryNews=(categoryId,categoryName)=>{
     const URL=`https://openapi.programming-hero.com/api/news/category/${categoryId}`
     fetch(URL)
     .then(res=>res.json())
-    .then(data=>console.log(data))
-
+    .then(data=>displayAllNews(data.data,categoryName))
+}
+const displayAllNews=(data,categoryName)=>{
+    // console.log(data.data.length);
+    document.getElementById('category-name-alert').innerText=categoryName;
+    document.getElementById('item-found').innerText=data.length;
+    console.log(data);
+    // data.forEach(singleNews => {
+    //     console.log(singleNews);
+    // });
 }
